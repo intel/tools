3a2911399e628934b0452aeae09c942b5d7fdfa6
Steven Robertson <steven@nervanasys.com>
2019-01-07 15:46:43 -0800

    made batch size variable

--- tensortuner/src/run_tuna_mkl.sh
+++ tensortuner/src/run_tuna_mkl.sh
@@ -3,6 +3,7 @@ set -e
 
 if [ $# -ne 3 ];
 then
+  # TODO: get rid of batch_size since it's an optimization param now?
   echo "run_tuna_mkl.sh <model> <batch_size> <cpu>"
   echo "<cpu> options: skl"
   echo "<model> options come from running this: python run_single_node_benchmark.py -h"
@@ -64,6 +65,7 @@ else
   LOG=${LOG_DIR}/${cpu}/${CFG_OUTPUT}/${CFG_SEARCH}/mkl/${model}.train.tuna.${CFG_SEARCH}.log
 fi
 
+set -x
 INIT_RADIUS=${CFG_INIT_RADIUS} \
 STRATEGY=${CFG_SEARCH}.so LAYERS=log.so \
 LOG_FILE=${LOG} \
@@ -72,13 +74,14 @@ ${HARMONY_HOME}/bin/tuna \
 -i=interop,${inter_op_min},${inter_op_max},${inter_op_step} \
 -i=intraop,${intra_op_min},${intra_op_max},${intra_op_step} \
 -i=omp,${omp_min},${omp_max},${omp_step} \
+-i=batch_size,8,2048,8 \
 -m=${CFG_OUTPUT} python ${TF_CNN_DIR}/run_single_node_benchmark.py \
 --cpu ${cpu} --mkl=True \
 --forward_only=${CFG_INFERENCE} --single_socket=${CFG_INFERENCE} \
 --num_warmup_batches=0 --distortions=False --optimizer=sgd \
---batch_size=${batch_size} \
 --data_format=NCHW --model=${model} \
 --num_batches=100 \
 --num_omp_threads=%omp \
 --num_inter_threads=%interop \
 --num_intra_threads=%intraop \
+# --batch_size=${batch_size} \

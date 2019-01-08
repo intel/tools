283da83d9b273e5b31cc94172f52b94a91c0038e
Steven Robertson <steven@nervanasys.com>
2019-01-07 17:09:24 -0800

    working on checking for error in underlying python script

--- tensortuner/src/run_tuna_mkl.sh
+++ tensortuner/src/run_tuna_mkl.sh
@@ -10,8 +10,7 @@ then
 fi
 
 model=$1
-batch_size=$2
-cpu=$3
+cpu=$2
 
 #############################
 # Configs
@@ -19,6 +18,8 @@ cpu=$3
 HOME_DIR=${HOME:-/home/user}
 LOG_DIR=${LOG_DIR:-/tmp}
 TF_CNN_DIR=${TF_CNN_DIR:-${HOME_DIR}/private-tensorflow-benchmarks/scripts/tf_cnn_benchmarks}
+# enables checking for python script error
+CHECK_FOR_ERROR=${CHECK_FOR_ERROR:-False}
 
 #############################
 # Tuna configs
@@ -57,11 +58,34 @@ else
   inter_op_step=1
 fi
 
+batch_size_min=8
+batch_size_max=2048
+batch_size_step=8
+
 if [ "$CFG_INFERENCE" = "True" ];
 then
   LOG=${LOG_DIR}/${cpu}/${CFG_OUTPUT}/${CFG_SEARCH}/mkl/${model}.inference.tuna.${CFG_SEARCH}.log
 else
   LOG=${LOG_DIR}/${cpu}/${CFG_OUTPUT}/${CFG_SEARCH}/mkl/${model}.train.tuna.${CFG_SEARCH}.log
+fi
+
+# tuna doesn't throw an error if the underlying script were to break, so we verify it can run once
+# TODO: is there a way tuna can do this for us??
+if [ "$CHECK_FOR_ERROR" = "True" ];
+then
+  echo "Trying underlying script, checking for error..."
+  python ${TF_CNN_DIR}/run_single_node_benchmark.py \
+  --cpu ${cpu} --mkl=True \
+  --forward_only=${CFG_INFERENCE} --single_socket=${CFG_INFERENCE} \
+  --num_warmup_batches=0 --distortions=False --optimizer=sgd \
+  --data_format=NCHW --model=${model} \
+  --num_batches=100 \
+  --num_omp_threads=${omp_min} \
+  --num_inter_threads=${inter_op_min} \
+  --num_intra_threads=${intra_op_min} \
+  --batch_size=${batch_size_min}
+  echo $?
+  AA
 fi
 
 INIT_RADIUS=${CFG_INIT_RADIUS} \
@@ -72,7 +96,7 @@ ${HARMONY_HOME}/bin/tuna \
 -i=interop,${inter_op_min},${inter_op_max},${inter_op_step} \
 -i=intraop,${intra_op_min},${intra_op_max},${intra_op_step} \
 -i=omp,${omp_min},${omp_max},${omp_step} \
--i=batch_size,8,2048,8 \
+-i=batch_size,${batch_size_min},${batch_size_max},${batch_size_step} \
 -m=${CFG_OUTPUT} python ${TF_CNN_DIR}/run_single_node_benchmark.py \
 --cpu ${cpu} --mkl=True \
 --forward_only=${CFG_INFERENCE} --single_socket=${CFG_INFERENCE} \
@@ -81,4 +105,5 @@ ${HARMONY_HOME}/bin/tuna \
 --num_batches=100 \
 --num_omp_threads=%omp \
 --num_inter_threads=%interop \
---num_intra_threads=%intraop
+--num_intra_threads=%intraop \
+--batch_size=$batch_size

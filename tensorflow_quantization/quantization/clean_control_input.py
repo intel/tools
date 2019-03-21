c662e0f97a45f7c3b4432446561fdea0bf75c767
Steven Robertson <srtrumpetaggie@gmail.com>
2019-03-20 17:13:53 -0700

    added tests for clean_control_input

--- tensorflow_quantization/quantization/clean_control_input.py
+++ tensorflow_quantization/quantization/clean_control_input.py
@@ -19,6 +19,7 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+import sys
 
 from tensorflow.core.framework import graph_pb2
 from tensorflow.python.platform import app
--- tensorflow_quantization/tests/unit/quantization/test_clean_control_input.py
+++ tensorflow_quantization/tests/unit/quantization/test_clean_control_input.py
@@ -2,27 +2,37 @@ import pytest
 import sys
 from mock import patch
 
+from quantization.clean_control_input import main
 from test_utils.io import catch_stdout
 
 
+@pytest.fixture()
+def mock_gfile(patch):
+    return patch("gfile")
+
+
+@pytest.fixture(autouse=True)
+def mock_flags(patch):
+    return patch("FLAGS")
+
+
 @pytest.fixture
-def mock_sys_args(patch):
-    return patch("sys.argv")
+def mock_graph_pb2(patch):
+    return patch("graph_pb2")
 
 
-def test_main(mock_sys_args):
+def test_main(mock_gfile, mock_graph_pb2):
     """Asserts passing in valid input results in return of 0"""
-    mock_sys_args = ["--input", "/tmp"]
-    import clean_control_input
-    clean_control_input.main(["--flag", "arg"])
+    mock_gfile.Exists.return_value = True
+    assert main(["--flag", "arg"]) == 0
 
 
-def test_main_bad_flags(mock_sys_args):
+def test_main_bad_flags(mock_gfile, mock_flags):
     """Asserts passing in bad input causes return of -1"""
-    mock_sys_args = ["--input", "/notafile"]
-    import clean_control_input
+    mock_gfile.Exists.return_value = False
+    mock_flags.input = "/notafile"
     with catch_stdout() as output:
-        func_return = clean_control_input.main(["--flag", "arg"])
+        func_return = main(["--flag", "arg"])
         output = output.getvalue()
     assert func_return == -1
-    assert output == "Input graph file /notafile does not exist!"
+    assert output == "Input graph file '/notafile' does not exist!\n"
